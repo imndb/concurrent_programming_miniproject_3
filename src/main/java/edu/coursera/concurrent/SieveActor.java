53c9487044cdbf67812dfc6482d6d2bdffd638e8
imndb <131273529+imndb@users.noreply.github.com>
2023-08-24 08:31:13 +0200

    added solution

--- src/main/java/edu/coursera/concurrent/SieveActor.java
+++ src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -2,24 +2,43 @@ package edu.coursera.concurrent;
 
 import edu.rice.pcdp.Actor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.rice.pcdp.PCDP.finish;
+
 /**
  * An actor-based implementation of the Sieve of Eratosthenes.
  *
- * TODO Fill in the empty SieveActorActor actor class below and use it from
  * countPrimes to determin the number of primes <= limit.
  */
 public final class SieveActor extends Sieve {
+
     /**
      * {@inheritDoc}
      *
-     * TODO Use the SieveActorActor class to calculate the number of primes <=
      * limit in parallel. You might consider how you can model the Sieve of
      * Eratosthenes as a pipeline of actors, each corresponding to a single
      * prime number.
      */
     @Override
     public int countPrimes(final int limit) {
-        throw new UnsupportedOperationException();
+        final SieveActorActor actor = new SieveActorActor(2);
+        finish(() -> {
+            for (int i = 3; i <= limit; i += 2) {
+                actor.send(i);
+            }
+        });
+
+        int n = 0;
+        SieveActorActor loopActor = actor;
+        while (loopActor != null) {
+            n += loopActor.primes.size();
+            loopActor = loopActor.nextActor;
+        }
+
+        return n;
+
     }
 
     /**
@@ -27,16 +46,41 @@ public final class SieveActor extends Sieve {
      * parallel.
      */
     public static final class SieveActorActor extends Actor {
+
+        private static final int MAX_PRIMES = 1_000;
+
+        private List<Integer> primes = new ArrayList<>();
+        private SieveActorActor nextActor;
+
+        SieveActorActor(final int primesCount) {
+            this.primes.add(primesCount);
+        }
+
         /**
          * Process a single message sent to this actor.
          *
-         * TODO complete this method.
          *
          * @param msg Received message
          */
         @Override
         public void process(final Object msg) {
-            throw new UnsupportedOperationException();
+
+            final int candidate = (Integer) msg;
+
+            final boolean isLocally =  primes.stream().allMatch( s -> candidate % s != 0);
+
+            if (isLocally) {
+                if (primes.size() < MAX_PRIMES) {
+                    primes.add(candidate);
+                } else if (nextActor != null) {
+                    nextActor.send(candidate);
+                } else {
+                    nextActor = new SieveActorActor(candidate);
+                }
+            }
+
         }
+
+
     }
 }
